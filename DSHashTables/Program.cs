ca40f4f546b5ad68facaa1dd6ed840e292d2029a
iamgauraav <iamgauraav@gmail.com>
2022-03-28 23:48:49 +0530

    Added Welcome message to master branch

---
+++ DSHashTables/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DSHashTables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to  Data Structure Hash Tables on master branch");
+        }
+    }
+}
